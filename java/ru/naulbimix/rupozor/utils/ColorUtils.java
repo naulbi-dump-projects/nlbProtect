0d12335c35739b99948ebf300dad0e6b8b4285f3
NaulbiMIX <48631163+NaulbiMIX@users.noreply.github.com>
2021-06-01 18:53:37 +0300

    Comment

--- java/ru/naulbimix/rupozor/utils/ColorUtils.java
+++ java/ru/naulbimix/rupozor/utils/ColorUtils.java
@@ -4,7 +4,7 @@ import net.md_5.bungee.api.ChatColor;
 
 public class ColorUtils {
 
-    public static String s(String s) {
+    public static String s(String s) { // бля название утилки не моё, но пастить новое не хочется
         return ChatColor.translateAlternateColorCodes('&', s);
     }
 }
